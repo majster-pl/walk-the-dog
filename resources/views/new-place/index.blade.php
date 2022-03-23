4114977398a3a27fbeca3a3115fe51d62e7aac90
majster-pl <waliczek.szymon@gmail.com>
2022-03-23 23:47:45 +0000

    Add checkBox to publish place if admin/editor

--- resources/views/new-place/index.blade.php
+++ resources/views/new-place/index.blade.php
@@ -9,8 +9,8 @@
                 <form class="my-3" action="{{ route('add-new-place') }}" method="post">
                     @csrf
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
-                            name="title" placeholder="Title" value="{{ old('title') }}">
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                            placeholder="Title" value="{{ old('title') }}">
                         <label for="title">Title</label>
                         @error('title')
                             <small class="text-danger">{{ $message }}</small>
@@ -33,6 +33,10 @@
                         @enderror
                     </div>
                     <div class="mb-3">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" id="publishedCheckbox" name="published" {{ Auth::user()->can('publish places') ? "checked" : ''}} {{ Auth::user()->can('publish places') ? "" : 'disabled'}}>
+                            <label class="form-check-label" for="publishedCheckbox" title="Only Editors and Admins can publish places, if you are not editor your place will be pending review.">Publish when submitted</label>
+                        </div>
                         <button type="submit" class="btn btn-success float-end">Submit</button>
                     </div>
                 </form>
