8b79218bafee17f94615c98112282db834753c66
majster-pl <waliczek.szymon@gmail.com>
2022-04-08 23:22:32 +0100

    Add sort by dropdown to places field

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -3,7 +3,19 @@
 @section('content')
     <div class="container py-4">
         <div class="card">
-            <div class="card-header fs-5">{{ __('All Places') }}</div>
+            <div class="card-header fs-5">{{ __('All Places') }} <div class="float-end">
+                    <div class="dropdown">
+                        <button class="btn btn-success text-white dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Sort by
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Likes</a></li>
+                            <li><a class="dropdown-item" href="#">Newest</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             <div class="card-body">
 
                 <div class="list-group">
