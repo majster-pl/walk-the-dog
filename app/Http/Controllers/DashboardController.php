087746d375fa0b7ef2a07a5914ae10a68f705958
majster-pl <waliczek.szymon@gmail.com>
2022-04-09 01:09:45 +0100

    Add filter to My Places dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,8 +12,26 @@ class DashboardController extends Controller
        $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
+        // return search page is search query present
+        if ($request->sort) {
+            switch ($request->sort) {
+                case 'created_at':
+                case 'status':
+                    $places = Place::orderByDesc($request->sort)->paginate(5);
+                    break;
+
+                default:
+                    $places = Place::orderBy($request->sort)->paginate(5);
+                    break;
+            }
+            // dd($places);
+            return view('dashboard.index', [
+                'places' => $places,
+            ]);
+        }
+
         $places = Place::where('user_id', auth()->id())->orderByDesc('created_at')->paginate(5);
         return view("dashboard.index", [
             'places' => $places
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,23 @@
 
     Places added by you
 </span>
+    <div class="float-end">
+        <div class="dropdown">
+            <button class="btn btn-sm btn-success text-white dropdown-toggle" type="button" id="dropdownMenuFilter1"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Sort by
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuFilter1">
+                <li><a class="dropdown-item" href="{{ route('dashboard') . '?sort=title' }}">Title</a></li>
+                <li><a class="dropdown-item" href="{{ route('dashboard') . '?sort=address_city' }}">City</a>
+                </li>
+                <li><a class="dropdown-item" href="{{ route('dashboard') . '?sort=created_at' }}">Newest</a>
+                <li><a class="dropdown-item" href="{{ route('dashboard') . '?sort=status' }}">Status</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
 @endsection
 @section('content-card')
     <div class="list-group">
