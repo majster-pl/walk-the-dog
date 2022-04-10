8244038541851ac0a615fde267cca6fa044f8ba6
majster-pl <waliczek.szymon@gmail.com>
2022-04-11 00:07:41 +0100

    Change working in contact page

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -23,11 +23,11 @@
                             </div>
                             <div class="form-floating mb-3">
                                 <textarea class="form-control" name="message" placeholder="Enter your message" id="ContactInputMessage"
-                                    style="height: 100px"></textarea>
+                                    style="height: 150px"></textarea>
                                 <label class="text-secondary" for="ContactInputMessage">Message</label>
                             </div>
                             <div class="mb-3">
-                                <small>If you want to <span class="text-success">become an editor</span> please tick "request editor" box below and write a bit about yourself and why you want to become an editor.</small>
+                                <small>If you want to <span class="text-success fw-bold">become an editor</span> please tick "request editor" box below and write a bit about yourself and why you want to become an editor, I'll reveiw your request as soon as posibble.</small>
                             </div>
                             <div class="form-check form-switch mb-3">
                                 <input class="form-check-input rounded-3" type="checkbox" id="ContactInputEditorRequest">
