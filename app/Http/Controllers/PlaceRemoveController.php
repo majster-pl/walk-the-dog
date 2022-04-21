51681c33904f842dfbef16451933f2d5650c94c0
majster-pl <waliczek.szymon@gmail.com>
2022-04-21 02:15:25 +0100

    add admin privilages to remove places

--- app/Http/Controllers/PlaceRemoveController.php
+++ app/Http/Controllers/PlaceRemoveController.php
@@ -18,7 +18,7 @@ class PlaceRemoveController extends Controller
     public function delete(Place $place)
     {
         $user = User::find(Auth::id());
-        if ($user->hasRole('editor') || ($place->user_id == $user->id)) {
+        if ($user->hasRole('editor|super-user') || ($place->user_id == $user->id)) {
             Place::destroy($place->id);
             return back()->with('success', 'Place removed successfully.');
         } else {
