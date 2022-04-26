f2d73ee592d942eaac6472180b222d40e5016289
majster-pl <waliczek.szymon@gmail.com>
2022-04-26 23:04:23 +0100

    Update text and spelling

--- resources/views/new-place-confirmation/index.blade.php
+++ resources/views/new-place-confirmation/index.blade.php
@@ -17,15 +17,14 @@
                 @else
                     <span class="fs-2">Thank you for taking your time to submit new place <i
                             class="fa fa-smile-o" aria-hidden="true"></i></span>
-                    <p class="fs-5">One of our editors will review your submittion with next 12 hours and publish
-                        it so everyone can see it!</p>
+                    <p class="fs-5">One of our editors will review it shortly and make it public so everyone can see it!</p>
                     <div class="row-">
                         <div class="col-12 col-md-4">
                             <img src="{{ asset('images/happy_dog.png') }}" class="img-fluid" alt="Happy dog">
                         </div>
                     </div>
-                    <a class="btn btn-success float-end text-white fw-bold" href="{{ route('add-new-place') }}">Add another</a>
-                    <p>Please get <a href="{{route('contact', 'editor')}}">in touch</a> if you want to become editor!</p>
+                    <a class="btn btn-success float-end text-white fw-bold" href="{{ route('add-new-place') }}">Add another place</a>
+                    <p>Please get <a href="{{route('contact', 'editor')}}">in touch</a> if you want to become an editor!</p>
                 @endif
             </div>
         </div>
