4d7465272618a3ec932bfedcb5a6b519139ce8d2
majster-pl <waliczek.szymon@gmail.com>
2022-04-27 23:31:25 +0100

    add status to preveiw & review page

--- resources/views/review-place/index.blade.php
+++ resources/views/review-place/index.blade.php
@@ -4,7 +4,9 @@
     <div class="container py-2">
         <a class="link-success" href="{{ URL::previous() }}">Go Back</a>
         <div class="card mt-2">
-            <div class="card-header fs-5">{{ __('Review Place') }}</div>
+            <div class="card-header fs-5">{{ __('Review Place') }}
+                <span class="float-end" style="font-size: .8rem">Status: <b>{{ $place->status }}</b></span>
+            </div>
             <div class="card-body">
                 @include('components.add-edit-place-form')
 
