bce8c9a8a93dee4e1da478c2ec73f0d17425b99a
majster-pl <waliczek.szymon@gmail.com>
2022-04-28 00:01:39 +0100

    refactor 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app', ['title' => 'Search results for: "'. Request::get('search'). '"' ])
+
+@section('content')
+    <div class="container pb-5 pt-4">
+        <h1>404 Page not found</h1>
+        <hr class="mt-0">
+            <div>
+                <p class="lead fs-4">We couldn't find the page you're looking for.</p>
+                <a class="btn btn-success text-white fw-bold" href="{{route('home')}}">Take me back to to homepage</a>
+
+            </div>
+
+    </div>
+@endsection
