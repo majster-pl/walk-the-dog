ffc59382af5969a235f678a4adee7f945420e744
majster-pl <waliczek.szymon@gmail.com>
2022-05-04 01:15:05 +0100

    Code clean up

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -67,12 +67,6 @@ class UserController extends AdminController
      */
     protected function form()
     {
-        $states = [
-            'on'  => ['value' => 1, 'text' => 'Yes', 'color' => 'success'],
-            'off' => ['value' => 0, 'text' => 'No', 'color' => 'danger'],
-        ];
-
-
         $form = new Form(new User());
 
         $form->text('name', __('Name'));
@@ -80,8 +74,6 @@ class UserController extends AdminController
         $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
         $form->password('password', __('Password'));
         $form->text('remember_token', __('Remember token'));
-
-        // $form->switch('isEditor', __('Editor'))->states($states);
 
         return $form;
     }
